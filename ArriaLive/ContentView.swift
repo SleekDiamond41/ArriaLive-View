1123bc910dc7d03f71176b4b8357ecc266813732
Michael Arrington <michael@clevercoding.com>
2019-09-07 16:50:01 -0600

    Initial Commit

---
+++ ArriaLive/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ArriaLive
+//
+//  Created by Michael Arrington on 9/7/19.
+//  Copyright © 2019 Duct Ape Productions. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
